007e4399a4a938ec36272c1e21de1f3f928789c3
Gangadhar C <gangadharganga90@gmail.com>
2023-03-31 16:04:45 +0530

    Time To Process License Plate Problem With Question

--- ProgradWeek2-Assesment/Program.cs
+++ ProgradWeek2-Assesment/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.ComponentModel;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace ProgradWeek2_Assesment
@@ -26,6 +27,23 @@ namespace ProgradWeek2_Assesment
     LicensePlate("2-4A0r7-4k", 3) ➞ "24-A0R-74K"
 
     LicensePlate("nlj-206-fv", 3) ➞ "NL-J20-6FV"*/
+
+       /* You have to get a new driver's license. You show up at the office at the same time as four other people. The office says they will see everyone in alphabetical order and it takes 20 minutes for them to process each new license. All of the agents are available now, and they can each see one customer at a time. How long will it take for you to walk out with your new license?
+
+    Your input will be a string of your name me, an integer of the number of available agents, 
+            and a string of the other four names separated by spaces others.
+
+    Return the number of minutes it will take to get your license.
+
+    Examples
+    License("Eric", 2, "Adam Caroline Rebecca Frank") ➞ 40
+    // As you are in the second group of people.
+
+    License("Zebediah", 1, "Bob Jim Becky Pat") ➞ 100
+    // As you are the last person.
+
+    License("Aaron", 3, "Jane Max Olivia Sam") ➞ 20
+    // As you are the first.*/
         static string ReorderedDMVNumber(string DMVNumber,int GroupNumber)
         {
             string reorderLicenceNumber="";
@@ -78,8 +96,7 @@ namespace ProgradWeek2_Assesment
         {
             Console.WriteLine(TimeToProcessLicensePlate("Eric", 2, "Adam Caroline Rebecca Frank"));
             Console.WriteLine(TimeToProcessLicensePlate("Zebediah", 1, "Bob Jim Becky Pat"));
-            Console.WriteLine(TimeToProcessLicensePlate("Aaron", 3, "Jane Max Olivia Sam"));
-
+            Console.WriteLine(TimeToProcessLicensePlate("Aaron", 3, "Jane Max Olivia Sam"))
         }
 
         static void Main(string[] args)
